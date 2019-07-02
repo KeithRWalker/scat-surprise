3ba888f57a6beb700541f0f83a35e3303ffc3e31
Keith Walker <Keithrwalker418@gmail.com>
2019-07-02 18:59:54 -0500

    auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
+import firebase from 'firebase/app';
 
 import Auth from '../components/Auth/Auth';
 import Home from '../components/Home/Home';
-import MyNav from '../components/MyNav/MyNav';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
+
     return (
       <div className="App">
-        <MyNav />
+        <MyNavbar authed={authed} />
+        {loadComponent()}
       </div>
     );
   }
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  };
+
   render() {
     return (
       <div className="Auth">
         <h1>Auth</h1>
-        <button className="btn btn-secondary">Auth Button</button>
+        <button className="btn btn-secondary" onClick={this.loginClickEvent}>Login with Google</button>
       </div>
     );
   }
